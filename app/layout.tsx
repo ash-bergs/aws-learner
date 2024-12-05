c487137e2d5388731980d42c3bcd09ec8c7bcb0d
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-12-05 12:10:34 -0500

    fix: rm unused local fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,6 @@ import './globals.css';
 
 import '@fontsource/poppins/index.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-
 export const metadata: Metadata = {
   title: 'Habit Nest',
   description: 'Stay on track with your habits',
