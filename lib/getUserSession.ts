dbe1665d7d4be107ea6722e6aed02a20a27cd805
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-12-27 00:25:45 -0500

    wip: connect supabase
    still deciding if I want to go this route, branch is just a POC

--- lib/getUserSession.ts
+++ lib/getUserSession.ts
@@ -2,6 +2,7 @@
 
 import { createSupabaseServerClient } from './supabase/server';
 
+// allows us to secure pages and routes based on the presence of a user session
 export default async function getUserSession() {
   const supabase = await createSupabaseServerClient();
 
