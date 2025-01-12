41a4ea65618e0e5a6271a0876861b5503b2a9186
Ash <65979049+ash-bergs@users.noreply.github.com>
2025-01-11 23:41:21 -0500

    fix: simplify text editor
    simplifies this, no given height we'll just let it grow as needed - we could consider this for the tasks too

--- app/components/TextEditor/TextEditor.tsx
+++ app/components/TextEditor/TextEditor.tsx
@@ -1,30 +1,21 @@
 'use client';
 
-import Placeholder from '@tiptap/extension-placeholder';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import { useNoteStore } from '@/lib/store/note';
 import Toolbar from './Toolbar';
+import { useNoteStore } from '@/lib/store/note';
 
+const extensions = [StarterKit];
 /**
  * A basic text editor component using tiptap.
  *
  * @returns {React.ReactElement} The JSX element representing the text editor.
  */
-const TextEditor = (): React.ReactElement => {
+const TextEditor = () => {
   const { addNote } = useNoteStore();
 
-  // following these docs: https://tiptap.dev/docs/editor/extensions/functionality/placeholder
-  // but this doesn't appear to be working as expected - wonder why?
-  // TODO: check versions, check any next js configs that might be causing issues
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Placeholder.configure({
-        placeholder: 'Write your notes here!',
-      }),
-    ],
-    content: '',
+    extensions,
   });
 
   const saveNote = () => {
@@ -42,9 +33,8 @@ const TextEditor = (): React.ReactElement => {
     <div className="rounded-md p4 mt-6">
       <Toolbar editor={editor} />
       <EditorContent
+        className="bg-white border rounded-b p-2"
         editor={editor}
-        //className="min-h-[200px] focus:outline-none active:outline-none"
-        className="h-64 overflow-y-auto bg-white border rounded-b p-2"
       />
       <button
         className="w-full text-center font-bold py-2 px-4 mt-4 rounded-md
