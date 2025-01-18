03111210f8869c02bc6ab257f7d8107d3fd4077d
Ash <65979049+ash-bergs@users.noreply.github.com>
2025-01-18 13:53:55 -0500

    feat: next-auth v5 (beta)

--- app/api/[auth]/[...nextauth]/route.ts
+++ app/api/[auth]/[...nextauth]/route.ts
@@ -1,9 +1,2 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '@/utils/authOptions';
-// export default NextAuth(authOptions);
-
-// Ran into some issues here:
-// as I forgot about updates to how api routes are handled in App router
-// and that the options have a strong type from Next Auth now
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+import { handlers } from '@/auth';
+export const { GET, POST } = handlers;
