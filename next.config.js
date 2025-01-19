aeb229d808da070fe037c726841373eb238c005e
Ash <65979049+ash-bergs@users.noreply.github.com>
2025-01-19 15:02:38 -0500

    test: prepend correctly
    forgot some places

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
+    MY_APP_DATABASE_URL: process.env.MY_APP_DATABASE_URL,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
