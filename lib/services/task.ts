1446275f0776bd7f03e51bf19287b75b1fe6207a
Ash <65979049+ash-bergs@users.noreply.github.com>
2025-02-16 01:31:54 -0500

    chore: rm console logs

--- lib/services/task.ts
+++ lib/services/task.ts
@@ -13,7 +13,6 @@ export class TaskService {
     try {
       const response = await fetch(`/api/tasks?userId=${userId}`);
       if (!response.ok) throw new Error('Failed to fetch tasks');
-      console.log('Look at that, a response from the API: ', response);
       return await response.json();
     } catch (error) {
       console.error('Failed to fetch tasks:', error);
@@ -26,7 +25,6 @@ export class TaskService {
         `/api/tasks?userId=${userId}&tagId=${tagId}`
       );
       if (!response.ok) throw new Error('Failed to fetch tasks');
-      console.log('Look at that, a response from the API: ', response);
       return await response.json();
     } catch (error) {
       console.error('Failed to fetch tasks:', error);
@@ -45,7 +43,6 @@ export class TaskService {
 
       if (!response.ok) throw new Error('Failed to add task');
       const newTask = await response.json();
-      console.log('Lookie here, a new task: ', newTask);
       return newTask;
     } catch (error) {
       console.error('Failed to add task:', error);
@@ -61,10 +58,8 @@ export class TaskService {
         },
         body: JSON.stringify({ id: id }),
       });
-      console.log('RESPONSE: ', response);
       if (!response.ok) throw new Error('Failed to delete tasks');
       const deletedTasks = await response.json();
-      console.log('Lookie here, a deleted task: ', deletedTasks);
       return deletedTasks;
     } catch (error) {
       console.error('Failed to delete tasks:', error);
@@ -80,10 +75,8 @@ export class TaskService {
         },
         body: JSON.stringify({ id: taskIds }),
       });
-      console.log('RESPONSE: ', response);
       if (!response.ok) throw new Error('Failed to delete tasks');
       const deletedTasks = await response.json();
-      console.log('Lookie here, a deleted task: ', deletedTasks);
       return deletedTasks;
     } catch (error) {
       console.error('Failed to delete tasks:', error);
@@ -116,29 +109,7 @@ export class TaskService {
       await db.tasks.update(id, updatedTask);
     }
   };
-  /**
-   * Update the color of a task background in the UI
-   * @param {string} id - The id of the task to update
-   * @param {string} color - The color to update the task with
-   */
-  updateTaskColor = async (id: string, color: string) => {
-    const task = await db.tasks.get(id);
 
-    if (task) {
-      const updatedTask = { ...task, color, dateUpdated: new Date() };
-      await db.tasks.update(id, updatedTask);
-      return updatedTask;
-    }
-
-    console.warn(`Task with id ${id} not found - color not updated 😢`);
-  };
-  // TODO: remove
-  updateTaskDate = async (id: string) => {
-    const task = await db.tasks.get(id);
-    if (!task) return console.warn(`No task with ${id} to update :(`);
-
-    await db.tasks.update(id, { dateUpdated: new Date() });
-  };
   updateTaskDueDate = async (id: string, dueDate: Date) => {
     const task = await db.tasks.get(id);
     if (!task) return console.warn(`No task with ${id} to update :(`);
